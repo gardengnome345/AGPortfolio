66db1a1f014375014620e0160c346d9a882bf1d7
Alexander Garyaev <125agaryaev@frhsd.com>
2024-10-10 11:39:27 -0400

    finished

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 function school()
 {
-
+    window.location.href = "#SchoolDiv";
 }
 function sports()
 {
-
+    window.location.href = "#SportsDiv";
 }
 function work()
 {
-
+    window.location.href = "#WorkDiv";
 }
